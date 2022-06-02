916529407119345c8946101ff07cbf3dbe654391
Nicholas <nickifeajika@gmail.com>
2022-06-02 17:21:32 +0100

    Hello world using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	puts("Programming is like building a multilingual puzzle\n");
+/* Entry point */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
