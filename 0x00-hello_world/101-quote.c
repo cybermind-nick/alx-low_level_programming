748d2da01dccd756c445b77fb3b058e9f2edb35b
Nicholas <nickifeajika@gmail.com>
2022-06-02 18:13:30 +0100

    Write to standard error buffer, ie, buffer 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Description: Program writes a
+ * quote to the standard error buffer
+ * Return: Returns a 1 (Error) 
+ * */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
