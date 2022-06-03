92d6a45a89937e6ace78762d630f7b8666bab316
Nicholas <nickifeajika@gmail.com>
2022-06-03 23:20:15 +0100

    every possible two digit combo x2

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Description: Prints out a combo of
+ * two digit numbers preceeded by '00'
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int tens, units, t, u;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (units = '0'; units <= '9'; units++)
+		{
+			for (t = tens; t <= '9'; t++)
+			{
+				for (u = units + 1; u <= '9'; u++)
+				{
+					putchar(tens);
+					putchar(units);
+					putchar(' ');
+					putchar(t);
+					putchar(u);
+
+					if (!((tens != '9' && units != '9') && (t != '9' && u != '9')))
+					{
+                                        putchar(',');
+                                        putchar(' ');
+					}	
+				}
+				u = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
