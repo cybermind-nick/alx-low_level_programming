85246ddb2212ba27307852c53cf4c1934ec5339d
Nicholas <nickifeajika@gmail.com>
2022-06-03 17:35:10 +0100

    Print hex digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < (48 + 10); n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n < (97 + 26); n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
