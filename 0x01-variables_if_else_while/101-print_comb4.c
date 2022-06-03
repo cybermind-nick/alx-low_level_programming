a4fc896621ba0524c57c468b6fe9b98830266fa8
Nicholas <nickifeajika@gmail.com>
2022-06-03 22:36:56 +0100

    smallest thre digit combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Description: Program to print the smallest list of combinations
+ * ...of three digit numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int hundreds, tens, units;
+
+	for (hundreds = '0'; hundreds < '9'; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= '9'; tens++)
+		{
+			for (units = tens + 1; tens <= '9'; tens++)
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(units);
+
+				if (hundreds != '7' && tens != '8' && units != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
