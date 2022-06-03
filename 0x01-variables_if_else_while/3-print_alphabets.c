7b28bfddee6b31c1bf54e6a1bd3ea4389624fd40
Nicholas <nickifeajika@gmail.com>
2022-06-03 16:59:18 +0100

    print alphabet in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < (97 + 26); n++)
+	{
+		putchar(n);
+	}
+	for (n = 65; n < (65 + 26); n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
