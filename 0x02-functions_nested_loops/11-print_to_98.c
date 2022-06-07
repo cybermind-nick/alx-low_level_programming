f62a36b1afa655ed79233cc0b497a23910b1bb49
Nicholas <nickifeajika@gmail.com>
2022-06-08 00:27:15 +0100

    count to 98 ascending or descending

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - definition of the function
+ * @n: int - starting point
+ * Description: Print to 98 in ascending or descending order
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
+			n--;
+		}
+	else
+		printf("%d", n);
+	}
+}
