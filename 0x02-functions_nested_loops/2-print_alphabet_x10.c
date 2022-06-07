75c674016e89cff09127033176b5a82c7dbfc958
Nicholas <nickifeajika@gmail.com>
2022-06-07 18:31:17 +0100

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - define print_alphabet_x10 in main.h
+ * Description - call print_alphabet() 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
