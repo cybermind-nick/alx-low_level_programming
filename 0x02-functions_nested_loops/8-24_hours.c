25e7d606a71111b9b515dc2bfa302ced62edd930
Nicholas <nickifeajika@gmail.com>
2022-06-07 22:12:50 +0100

    counting every minute in the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - declared in "main.h"
+ * Description: Prints every minute in a day
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour_t, hour_u, min_t, min_u;
+
+	for (hour_t = '0'; hours <= '2'; hours++)
+	{
+		for (hour_u = '0'; hour_u <= '4'; hour_u++)
+		{
+			for (min_t = '0'; min_t <= '5'; min_t++)
+			{
+				for (min_u = '0'; min <= '9'; min_u++)
+				{
+					putchar(hour_t);
+					putchar(hour_u);
+					putchar(':');
+					putchar(min_t);
+					putchar(min_u);
+					putchar('\n');
+				}
+			}
+		}
+	}
+}
