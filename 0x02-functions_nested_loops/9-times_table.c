4ff398bb205deefd01c39147c13361ac04840db7
Nicholas <nickifeajika@gmail.com>
2022-06-07 23:15:50 +0100

    9x9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,8 +18,8 @@ void times_table(void)
 
 			if (result > 9)
 			{
-				_putchar('0' + (result / 10);
-				_putchar('0' + (result % 10);
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
 			}
 			else
 			{
