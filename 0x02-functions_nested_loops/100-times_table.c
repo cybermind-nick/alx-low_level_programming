2c10e9dbf9e6e0d4a2e038fc636048a6f2726e5b
Nicholas <nickifeajika@gmail.com>
2022-06-08 23:08:56 +0100

    print nxn times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints nxn times table
+ * @n: the table limit
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int row, col;
+
+	for (row = 0; row <= n; row++)
+	{
+		if (n < 0 || n > 15)
+			break;
+		for (col = 0; col <= n; col++)
+		{
+			int result = row * col;
+
+			if (result == 0)
+				_putchar('0');
+
+			_putchar(',');
+			_putchar(' ');
+
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
