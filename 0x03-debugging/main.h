84cb13ce4c504c8f20c78e5cae5159e1aa67aedb
Nicholas <nickifeajika@gmail.com>
2022-06-09 22:57:12 +0100

    declared print_remaining_days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -20,4 +20,23 @@ void positive_or_negative(int n);
  */
 
 int largest_number(int a, int b, int c);
+
+/**
+ * convert_day - gets the day relative to the number of days in a year
+ * @month: the month in integer count
+ * @day: the day of the month
+ * Return: day of the year
+ */
+
+int convert_day(int month, int day);
+
+/**
+ * print_remaining_days - takes a date and prints how many days
+ * are left in the year, taking leap years into account
+ * @month: Month in number format
+ * @day: day of the month
+ * Return: void
+ */
+
+void print_remaining_days(int month, int day, int year);
 #endif
