745e2699ebfcc1a576ed13611b7b4af92ddc677c
Nicholas <nickifeajika@gmail.com>
2022-06-09 21:29:53 +0100

    declared positive_or_negative()

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * positive_or_negative - checks if an int is positive or negative
+ * @n: int to be checked
+ * Return: void
+ */
+
+void positive_or_negative(int n);
+
+#endif
