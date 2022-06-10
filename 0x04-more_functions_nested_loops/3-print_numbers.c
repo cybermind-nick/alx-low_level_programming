252f0463cd9da3a383350d2f01a2ecf77420de86
Nicholas <nickifeajika@gmail.com>
2022-06-10 18:12:31 +0100

    print the 10 digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print all the digits
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+}
