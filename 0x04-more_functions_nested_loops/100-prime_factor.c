065a555b7108b1af125e2f592f31a84ca36ded0e
Nicholas <nickifeajika@gmail.com>
2022-06-11 00:36:22 +0100

    largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Print the prime factors
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long num = 612852475143;
+	unsigned long divisor = 2;
+
+	while (divisor < num)
+	{
+		if (num % divisor == 0)
+		{
+			num /= divisor;
+			divisor = 2; /* reset divisor */
+		}
+		else
+			divisor++; /* increment divisor */
+	}
+
+	printf("%lu\n", num); /* what is left in num is the largest factor */
+
+	return(0);
+
+}
