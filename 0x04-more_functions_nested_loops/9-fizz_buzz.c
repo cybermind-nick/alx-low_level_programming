bce02326f2066dbc084468c13bf2d86be3859c6b
Nicholas <nickifeajika@gmail.com>
2022-06-10 22:19:34 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,26 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
 		else
-			printf("%d ", i);
+		{
+			printf("%d",i);
+		}
 
 		if (i < 100)
 			printf(" ");
 	}
-	printf('\n');
+	printf("\n");
+
 	return (0);
 }
