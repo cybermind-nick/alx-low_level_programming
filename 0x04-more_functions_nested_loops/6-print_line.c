d3a8df3f4ed5c5a87b6bed470af02ab5e0c95aec
Nicholas <nickifeajika@gmail.com>
2022-06-10 19:19:46 +0100

    print underscore n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print underscore n times
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
