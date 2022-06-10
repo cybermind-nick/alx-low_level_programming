44f1e20fb179ca65bab226c8f5bfcae547fdb5db
Nicholas <nickifeajika@gmail.com>
2022-06-10 21:47:56 +0100

    print an nxn sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print nxn square
+ * @n: size of sqaure to print
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int h, b;
+
+	for (h = 0; h < n; h++)
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
