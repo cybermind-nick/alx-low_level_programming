a47c62be4020a77c4a6b69932b692f67d843abad
Nicholas <nickifeajika@gmail.com>
2022-06-13 23:10:11 +0100

    reverse a string itself

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+include "main.h"
+
+/**
+ * rev_string - reverse a string itself (modify, not print)
+ * @s: the string to be modified
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char *temp;
+	int len = 0;
+
+	while (*s = '\0')
+	{
+		len++;
+		++s;
+	}
+
+	while (len > 0)
+	{
+		temp += *s;
+		len--;
+		s--;
+	}
+
+	s = temp;
+}
