233a43e9b77ee4c5ce1e1076aa910ad9bf9a72cf
Nicholas <nickifeajika@gmail.com>
2022-06-14 21:28:04 +0100

    print even 2 characters in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters in a string every skip interval
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i <= '\0', i + 2)
+	{
+		_putchar(*(str + i));
+	}
+}
