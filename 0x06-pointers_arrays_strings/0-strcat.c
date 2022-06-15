f288d856dbbfb77b057dcd2962f8a14e1014f8d9
Nicholas <nickifeajika@gmail.com>
2022-06-15 23:47:40 +0100

    string concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,23 +11,19 @@ char * _strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	while(1)
+	while (dest[i] != '\0')
 	{
-		if (*(dest + i) == '\0')
-		{
-			while (*(src + j) != 0)
-			{
-				*(dest + i) = *(src + j);
-				i++;
-				j++;
-			}
-		}
-		else
-		{
-			i++;
-			continue;
-		}
+		i++;
 	}
 
-	return dest;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
