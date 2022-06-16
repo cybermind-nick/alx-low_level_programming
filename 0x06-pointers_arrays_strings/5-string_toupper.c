061a46443a6dc2f52c5b2326f3c526b50afc016d
Nicholas <nickifeajika@gmail.com>
2022-06-16 23:08:39 +0100

    all letter to UPPERCASE

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts all strings to Uppercase
+ * @s: string to be operated on
+ *
+ * Return: str (modified string)
+ */
+
+char *string_toupper(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s = *s - 32;
+	}
+
+	return (s);
+}
