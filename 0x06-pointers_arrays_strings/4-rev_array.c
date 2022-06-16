dff5cd2d198ef6fa55be6e877c0eddf2760e913f
Nicholas <nickifeajika@gmail.com>
2022-06-16 22:44:18 +0100

    reverse an array of n elements

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while (a[i] != a[n - i])
+	{
+		temp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = temp;
+
+		i++;
+	}
+}
