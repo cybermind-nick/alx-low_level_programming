32fc5704b6674f72e161f6cde938ea962993b991
Nicholas <nickifeajika@gmail.com>
2022-06-16 16:54:31 +0100

    concat n bytes of src to dest

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,24 +13,18 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (*dest != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
-		s++;
 	}
 
-	while (n >= 0)
+	while (src[j] != src[n])
 	{
-		if (*src != 0)
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-			n--;
-		}
-		else
-			break
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
