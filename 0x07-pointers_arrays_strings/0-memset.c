86f841cd05a5363332b442234c762cfa191a1dc6
Nicholas <nickifeajika@gmail.com>
2022-06-21 00:15:27 +0100

    set bytes to a constant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,14 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	int bytes = n; /* Force bytes to be positive only */
 	int i;
 
-	for (i = 0; s[i] != '\0' || i < n; i++)
-	{
-		s[i] = b;
-	}
+	if (bytes > 0)
+		for (i = 0; s[i] != '\0' || i < n; i++)
+		{
+			s[i] = b;
+		}
 
-	return (s)
+		return (s)
 }
