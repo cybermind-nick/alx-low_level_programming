bd287e3eb41736f9109e7963866961f23cec91f0
Nicholas <nickifeajika@gmail.com>
2022-06-20 22:35:54 +0100

    set bytes to a constant

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - set bytes if a string in memory
+ * ...to a constant byte 
+ * @s: string in memory
+ * @b: byte contant
+ * @n: number of bytes in 's' to fill with the constant
+ * Return: char *s - filled up string memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0' || i > n; i++)
+	{
+		*(s + i) = b;
+	}
+}
