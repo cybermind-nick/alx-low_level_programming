c519a3e223b0d45c84fbf1879dbdbcfd6618739e
Nicholas <nickifeajika@gmail.com>
2022-06-21 00:24:16 +0100

    copy n bytes of character from source to destination

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * memcpy - copy one string to another from memory
+ * @dest: location to be copied to
+ * @src: location to be copied from
+ * @n: number of bytes to be copied
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int bytes = n;
+	int i;
+
+	if (bytes > 0)
+	{
+		for (i = 0; i < bytes; i++)
+		{
+			dest[i] = src[i];
+		}
+	}
+
+	return (dest);
+}
