d354322ac96788186ceabce1a46bced4c83759a8
Nicholas <nickifeajika@gmail.com>
2022-06-21 23:33:28 +0100

    search for substring in string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,30 +6,38 @@
  * @haystack: string to search
  * @needle: substring to be searched for
  *
- * Return: Pointer to beginning of substring
+ * Return: Pointer to beginning of substring if there is a match
+ * or NULL if no match
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j;
+	int i = 0, y, x;
 
-	if (needle[0] == '\0')
+	if (needle[0] == '\0')/* check if the needle exist,esle return haystack*/
 		return (haystack);
 
-	while (haystack[i] != '\0')
+	while (haystack[i] != '\0') /* loop through haystack */
 	{
+		/* if a byte matches first char of a needle */
+		/* loop through needle until match ends */
 		if (haystack[i] == needle[0])
 		{
-			for (j = 1; needle[j] = '\0'; j++)
+			x = i, y = 0;
+			while (needle[y] != '\0')
 			{
-				if (haystack[i + j] != needle[j])
+				if (haystack[x] == needle[y])
+					x++, y++;
+				else
 					break;
-				else if (haystack[i + j] == needle[j] && needle[j + 1] == '\0')
-					return (&haystack[i]);
+			} /* if matched throughout, return haystack */
+
+			if (needle[y] == '\0')
+			{
+				return (haystack + i);
 			}
 		}
 		i++;
 	}
-
-	return (NULL);
+	return (NULL); /* if no match at all*/
 }
