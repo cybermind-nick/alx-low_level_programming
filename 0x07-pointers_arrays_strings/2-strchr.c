9daee8df8dec32835e6361e1eaab2ff7f2cd8c5c
Nicholas <nickifeajika@gmail.com>
2022-06-21 22:04:31 +0100

    check if char c is in string s

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - checks for char 'c' in string 's'
+ * @s: string to be checked
+ * @c: char to c
+ *
+ * Return: pointer to char or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)/* find match */
+	{
+		i++;
+	}
+
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
