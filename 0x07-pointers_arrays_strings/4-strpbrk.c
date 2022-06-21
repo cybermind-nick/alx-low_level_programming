f1f9324aeea4aa62ea8fe9ac882009b73c731109
Nicholas <nickifeajika@gmail.com>
2022-06-21 23:02:08 +0100

    return pointer to first occurence of a substring

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * strpbrk - return first occurence of accept and break
+ * @s: string to check
+ * @accept: source string
+ *
+ * Return: char pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s = &s[i]; /* set pointer (the string) to first occurence of target*/
+				return (&s[i]);
+
+			}
+		}
+		i++;
+	}
+
+	return (NULL)
+}
