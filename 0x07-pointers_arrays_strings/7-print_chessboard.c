697d76acfd08f6b478c570d1205b4adb21263a87
Nicholas <nickifeajika@gmail.com>
2022-06-21 23:42:06 +0100

    print a mock chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a caricature chessboard
+ * @a: 8x8 two-dimensional array to represent the chess board
+ *
+ * Return: void
+ */
+
+void print_chessboard(int (*a)[8])
+{
+	int i = 0, j;
+
+	while (*(a + i) != 0)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(a + i)[j]);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
