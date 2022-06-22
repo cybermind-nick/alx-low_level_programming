7718ebebc756b01f7edd7f0f1060c536f9eb811c
Nicholas <nickifeajika@gmail.com>
2022-06-22 01:16:56 +0100

    set a string to another value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - set a string to another value
+ * @s: pointer to string to be set
+ * @to: string to be set to
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
