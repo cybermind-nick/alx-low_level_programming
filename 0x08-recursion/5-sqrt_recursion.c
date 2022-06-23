ebd800f0cc2ed9d61b6c0b4dbe092028c148e1b7
Nicholas <nickifeajika@gmail.com>
2022-06-24 00:55:18 +0100

    find the natural root of n using recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * find_root - searches for the root of n if it exists
+ * @n: root to search for
+ * @root: potential roots (increment)
+ *
+ * Return: int - root (if exits) else -1 or 0
+ */
+
+int find_root(int n, root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1)); /* root = 0 is the starting state */
+}
+
+/**
+ * _sqrt_recursively - calculate the natural root of a number recursively
+ * @n: the number to find the natural root of
+ *
+ * Return - int (root - it exist) or -1
+ */
+
+int _sqrt_recursively(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (find_root(n, 0));
+}
