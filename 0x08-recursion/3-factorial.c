8f749a8c405379e2d37861237ca76364380ab7a9
Nicholas <nickifeajika@gmail.com>
2022-06-23 23:47:00 +0100

    calculate the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - calculate the factorial of a number
+ * @n: number
+ *
+ * Return: int - calculated factorail
+ */
+
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+		return (1);
+
+	return (n * factorial(n -1));
+}
