47b9ad533d6276196410da0b53ba305ce7988ae5
Nicholas <nickifeajika@gmail.com>
2022-06-24 19:03:56 +0100

    argument counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description: print the number of arguments supplied...
+ * ...to main
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return (0)
+ */
+
+int main(int argc, int *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
