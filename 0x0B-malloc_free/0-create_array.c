1546c6fbffb082cd8f359942ac571752a3ea1f95
Nicholas <nickifeajika@gmail.com>
2022-06-27 19:09:34 +0100

    create a character array and return a pointer to it

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return NULL;
 
-	c_arr = (char *)(malloc(sizeof(char) * n));
+	c_arr = (char *)(malloc(sizeof(char) * size));
 
 	if (c_arr == NULL)
 		return NULL;
