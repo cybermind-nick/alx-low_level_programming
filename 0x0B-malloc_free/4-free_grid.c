3f54c1f2ad0c13f7cc955fbe1e2131a464dbad86
Nicholas <nickifeajika@gmail.com>
2022-06-28 21:46:04 +0100

    free a grid space previously allocated

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free up memory of a number of rows (height)
+ * @grid: array to be operated on
+ * @height: number of row memory to deallocate
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
