c068c35450df3bc4f33077b553e2ab9a0271b236
Nicholas <nickifeajika@gmail.com>
2022-06-28 21:52:47 +0100

    free a grid space previously allocated

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -15,7 +15,7 @@ void free_grid(int **grid, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; a[i][j] == NULL; j++)
+		for (j = 0; grid[i][j] == NULL; j++)
 			free(grid[i][j]);
 		free(grid[i]);
 	}
