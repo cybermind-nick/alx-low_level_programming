db915791a08f269f69e655f68ec7b33098b12137
Nicholas <nickifeajika@gmail.com>
2022-06-29 21:39:35 +0100

    2-D word array

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - spilt a string into words and store in a 2-D array
+ * @str: string to spilt
+ *
+ * Return: 2-D array of words
+ */
+
+char **strtow(char *str)
+{
+	char **words;
+	char *word_store;
+	char **word_track; /* To hold the starting address of word store */
+	int i, j, size = 0;
+
+	word_track = word_store;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	/* Get the number of rows */
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (str[j] == " " || str[j] == '\0')
+			size++;
+	}
+
+	words = (char **)(malloc((sizeof(char *) * size) + 1));
+
+	if (words == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		*word_store = str[i];
+		word_store++;
+
+		if (str[i] == " " && str[i] != '\0')
+		{
+			word[i] = *word_track; /* POint to the start of word_store */
+			*word_track = NULL;
+			word_track = word_store; /* Set to new word_store location */
+		}
+		i++;
+	}
+
+	words[i + 1] = NULL; /* Set last element to NULL */
+
+	return (words);
+}
