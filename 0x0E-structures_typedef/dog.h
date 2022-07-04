30984f2c8a22e609fa0236354a3ab2ebabec4a1d
Nicholas <nickifeajika@gmail.com>
2022-07-04 23:37:10 +0100

    defined type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,9 @@
 #define DOG_H
 
 /**
- * dog - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-typedef struct dog dog;
+typedef struct dog dog_t;
 
 /**
  * struct dog - structure for dogs
