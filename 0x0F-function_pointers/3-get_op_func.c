351baa0478b4fc06d23aa7ea286e4d4711a9ea42
Nicholas <nickifeajika@gmail.com>
2022-07-09 00:36:12 +0100

    defined the get_op_func function to retrieve the correct operational function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - retrieve and execute the right arithmeic functon on the args
+ * @s: string of the operation
+ *
+ * Return: function pointer to operation
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
