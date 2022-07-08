2c262e8021d6dd8a7dc4aab4655a801ee7a96938
Nicholas <nickifeajika@gmail.com>
2022-07-08 17:18:05 +0100

    print_name using function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdio.h>
 
 /**
  * print_name - prints out the name of using a custom function
  * @name: string to be printed
- *
+ * @f: function pointer
  * Return: void
  */
 
