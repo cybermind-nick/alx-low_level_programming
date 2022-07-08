8a98de608d54315fe3b7fa8a3eef380baab322bb
Nicholas <nickifeajika@gmail.com>
2022-07-09 00:35:29 +0100

    defined all operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum of a & b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - addition
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference of a & b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - addition
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product of a & b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - addition
+ * @a: first int
+ * @b: second int
+ *
+ * Return: division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - addition
+ * @a: first int
+ * @b: second int
+ *
+ * Return: module of a mod b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
