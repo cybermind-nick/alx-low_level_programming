96ae890c3191d5257e6a25948c9f5002a55ae246
Nicholas <nickifeajika@gmail.com>
2022-07-18 23:44:38 +0100

    declared add_nodeint()

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,5 +21,6 @@ typedef struct listint_s
 /* Functions */
 size_t print_listint(const listint_t *head);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
