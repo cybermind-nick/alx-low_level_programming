beb108948cf3e4ee2b85a64004997979e7e6ae26
Nicholas <nickifeajika@gmail.com>
2022-07-19 20:56:25 +0100

    free memory allocated to individual nodes on a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint - free memory allocated to a list
+ * @head: start of list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	/* You must free each indiviual node in a linked list */
+	listint_t *ptr; /* Use to free each individual allocated node */
+
+	if (head == NULL)
+		exit(98);
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next; /* Move to next allocated node */
+		free(ptr); /* free current node */
+	}
+}
